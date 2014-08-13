38ae6d67cd60879724a517a9c54dbd491e6208a3
Gansperger István <gansperger@gmail.com>
2014-08-13 14:12:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# websharper.phonejs [![Build status]()](https://ci.appveyor.com/project/Jand42/websharper-phonejs)
+
+[Samples][samp] for [WebSharper][ws] bindings to PhoneJS.
+
+
+[samp]: http://intellifactory.github.io/samples.websharper.phonejs
+[ws]: http://websharper.com/
---
+++ websharper.reddit/Utils.fs
@@ -0,0 +1,13 @@
+﻿namespace websharper.nytimes
+
+open IntelliFactory.WebSharper
+
+[<JavaScript; AutoOpen>]
+module Utils =
+    module Array =
+        let Last (arr : 'a []) =
+            arr.[arr.Length - 1]
+
+    type 'T ``[]`` with
+        member a.Last = a.[a.Length - 1]
+       
